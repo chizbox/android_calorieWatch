f40b550b7841b5de338e0a4f73d582d8ace836c0
Chizbox <Chizbox@192.168.254.100>
2013-01-19 23:20:12 +0800

    sample

---
+++ src/me/micka/calorie_watch/FromChizboxPC.java
@@ -0,0 +1,13 @@
+package me.micka.calorie_watch;
+
+public class FromChizboxPC {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
